80218975e53632e4ed3775c7df222eadba49837c
WangYang <13503499+wangyang_go@user.noreply.gitee.com>
2024-06-19 17:26:35 +0800

    youhua

---
+++ wangyang/code_study_demo/Study.java
@@ -0,0 +1,50 @@
+/**
+ * ClassName: Study
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author WangYang
+ * @Create 2024/6/19 16:14
+ * @Version 1.0
+ */
+import java.io.File;
+import java.util.List;
+public class Study {
+    public static List<File> getUserDirectory(String path, List<File> list) {
+        
+        File file = new File(path);
+        if (!file.exists()) {
+            System.out.println("文件不存在!");
+            return list;
+        }
+
+        if (file.getName().contains("不算")) {
+            return list;
+        }
+
+        if (file.isDirectory() && file.getName().matches("\\d+")) {
+            list.add(file);
+            return list;
+        }
+
+        File[] files = file.listFiles();
+        if (files == null || files.length == 0) {
+            System.out.println("文件夹是空的!");
+            return list;
+        }
+
+        for (File file1 : files) {
+            if (file1.getName().contains("不算")) {
+                continue;
+            }
+            if (file1.isDirectory() && !file1.getName().matches("\\d+")) {
+                getUserDirectory(file1.getAbsolutePath(), list);
+                continue;
+            }
+            if (file1.isDirectory() && file1.getName().matches("\\d+")) {
+                list.add(file1);
+            }
+        }
+        return list;
+    }
+}
